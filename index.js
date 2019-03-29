24fe4ac86f3500bd738cce06c92e3e930d5904cc
Kent Quirk <kentquirk@gmail.com>
2019-03-29 14:56:17 -0400

    consistent function definition syntax

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const NAPU_PER_NDAU = 100000000
 
-function setCharAt (str, index, chr) {
+const setCharAt = (str, index, chr) => {
   if (index > str.length - 1) return str
   return str.substr(0, index) + chr + str.substr(index + 1)
 }
