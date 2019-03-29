791accc2e801c7176433a9882c1202fa96948aaa
Kent Quirk <kentquirk@gmail.com>
2019-03-29 11:22:16 -0400

    Add formatter function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const NAPU_PER_NDAU = 100000000
+
+function setCharAt (str, index, chr) {
+  if (index > str.length - 1) return str
+  return str.substr(0, index) + chr + str.substr(index + 1)
+}
+
 module.exports = {
   /**
    * This function will take a string passed in and truncate
@@ -13,5 +20,66 @@ module.exports = {
       return `${address.slice(0, 8)}...${address.slice(-8)}`
     }
     return address
+  },
+
+  /**
+   * This takes a number of napu, which should be an integer, plus a
+   * precision (number of decimal digits) and returns a string
+   * which is the human-readable value in ndau, rounded to that
+   * number of decimal digits. No floating point math is used.
+   */
+  formatNapuForDisplay: (napu, digits) => {
+    // fix digits if it's out of range (only 0-8 are permitted)
+    if (digits < 0) {
+      digits = 0
+    }
+    if (digits > 8) {
+      digits = 8
+    }
+
+    // if the number is negative, we'll need a sign, so get that
+    // out of the way first and then work in positive numbers
+    let sign = ''
+    if (napu < 0) {
+      let napu = -napu
+      let sign = '-'
+    }
+
+    let ndau = Math.floor(napu / NAPU_PER_NDAU)
+    let frac = napu % NAPU_PER_NDAU
+
+    // if digits == 0 then we want a rounded number of ndau,
+    // so let's do that before messing with frac
+    if (digits == 0) {
+      if (frac >= NAPU_PER_NDAU / 2) {
+        return sign + (ndau + 1).toString()
+      } else {
+        return sign + ndau.toString()
+      }
+    }
+
+    // now we have sign and ndau and decimal point
+    // now format the fractional value
+    // turn it into an 8-digit string with leading zeros if needed.
+    let fs = ('00000000' + frac.toString()).slice(-8)
+    // trim it to the right length
+    let fracs = fs.slice(0, digits)
+    // see if we might have to round it
+    if (digits < 8) {
+      let nextdigit = fs[digits]
+      if ('56789'.indexOf(nextdigit) !== -1) {
+        let newfrac = (parseInt(fracs) + 1).toString()
+        // if that overflowed, we need to adjust ndau
+        // (we rounded, say, 999 to 1000)
+        if (newfrac.length != fracs.length) {
+          newfrac = newfrac.slice(-digits)
+          ndau = ndau + 1
+        }
+        fracs = newfrac
+      }
+    }
+
+    // we finally have all the bits
+    return sign + ndau.toString() + '.' + fracs.toString()
   }
 }
--- test/index.js
+++ test/index.js
@@ -25,3 +25,68 @@ describe('Address related functions', () => {
     })
   })
 })
+
+describe('ndau formatting', () => {
+  describe('#formatNapuForDisplay', () => {
+    it('should format 1 ndau correctly as 1', () => {
+      expect(ndaujs.formatNapuForDisplay(100000000, 0)).to.equal('1')
+    })
+    it('should format 1.234 ndau with 0 as 1', () => {
+      expect(ndaujs.formatNapuForDisplay(123400000, 0)).to.equal('1')
+    })
+    it('should format 1.234 ndau with 1 as 1.2', () => {
+      expect(ndaujs.formatNapuForDisplay(123400000, 1)).to.equal('1.2')
+    })
+    it('should format 1.234 ndau with 3 as 1.234', () => {
+      expect(ndaujs.formatNapuForDisplay(123400000, 3)).to.equal('1.234')
+    })
+    it('should format 1.234 ndau with 8 as 1.23400000', () => {
+      expect(ndaujs.formatNapuForDisplay(123400000, 8)).to.equal('1.23400000')
+    })
+    it('should format 1.255 ndau with 1 as 1.3', () => {
+      expect(ndaujs.formatNapuForDisplay(125500000, 1)).to.equal('1.3')
+    })
+    it('should format 1.255 ndau with 2 as 1.26', () => {
+      expect(ndaujs.formatNapuForDisplay(125500000, 2)).to.equal('1.26')
+    })
+    it('should format 1.255 ndau with 3 as 1.255', () => {
+      expect(ndaujs.formatNapuForDisplay(125500000, 3)).to.equal('1.255')
+    })
+    it('should format 1.255 ndau with 4 as 1.2550', () => {
+      expect(ndaujs.formatNapuForDisplay(125500000, 4)).to.equal('1.2550')
+    })
+    it('should format 1.99 ndau with 0 as 2', () => {
+      expect(ndaujs.formatNapuForDisplay(199000000, 0)).to.equal('2')
+    })
+    it('should format 1.99 ndau with 1 as 2.0', () => {
+      expect(ndaujs.formatNapuForDisplay(199000000, 1)).to.equal('2.0')
+    })
+    it('should format 1.99 ndau with 2 as 1.99', () => {
+      expect(ndaujs.formatNapuForDisplay(199000000, 2)).to.equal('1.99')
+    })
+    it('should format 1.99 ndau with 3 as 1.990', () => {
+      expect(ndaujs.formatNapuForDisplay(199000000, 3)).to.equal('1.990')
+    })
+    it('should format 1 napu with 0 as 0', () => {
+      expect(ndaujs.formatNapuForDisplay(1, 0)).to.equal('0')
+    })
+    it('should format 1 napu with 2 as 0.00', () => {
+      expect(ndaujs.formatNapuForDisplay(1, 2)).to.equal('0.00')
+    })
+    it('should format 0 napu with 3 as 0.000', () => {
+      expect(ndaujs.formatNapuForDisplay(1, 3)).to.equal('0.000')
+    })
+    it('should format 10 napu with 7 as 0.0000001', () => {
+      expect(ndaujs.formatNapuForDisplay(10, 7)).to.equal('0.0000001')
+    })
+    it('should format 10 napu with 8 as 0.00000010', () => {
+      expect(ndaujs.formatNapuForDisplay(10, 8)).to.equal('0.00000010')
+    })
+    it('should format 10 napu with 9 as 0.00000010', () => {
+      expect(ndaujs.formatNapuForDisplay(10, 9)).to.equal('0.00000010')
+    })
+    it('should format 10 napu with -1 as 0', () => {
+      expect(ndaujs.formatNapuForDisplay(10, -1)).to.equal('0')
+    })
+  })
+})
