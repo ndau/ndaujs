8f7aaaa86fed57ddf592bb8e19ac7bf2c31e6dc6
Kristofer Pelchat <kris.pelchat@oneiro.io>
2019-08-21 15:13:52 -0400

    added ES6 class to index.js to make sure I can grab it from the top level

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const address = require('./src/keyaddress/address')
+const APIAddressHeader = require('./src/api/helpers/APIAddressHelper')
 
 module.exports = {
   truncateAddress: address.truncateAddress,
   formatNapuForDisplay: address.formatNapuForDisplay,
-  parseNdau: address.parseNdau
+  parseNdau: address.parseNdau,
+
+  APIAddressHeader
 }
