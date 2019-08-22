38b161c9f6436ed94084a6e0c66397c4cadea934
Kristofer Pelchat <kris.pelchat@oneiro.io>
2019-08-22 15:05:55 -0400

    make sure we run ALL tests and bring back the original implementation of truncateAddress formatNapuForDisplay and parseNdau

--- src/keyaddress/address.js
+++ src/keyaddress/address.js
@@ -1,19 +1,18 @@
 const constants = require('../constants/constants')
 
-/**
- * This function will take a string passed in and truncate
- * it down to 19 characters if it is more than that. This
- * was created for ndau addresses as they are 48 characters
- * and tend to cause issues when displaying them in the UI.
- *
- * @param {string} address ndau address to be truncated
- * @returns {string} truncated string
- */
-module.exports.truncateAddress = address => {
-  if (address && address.length > 19) {
-    return `${address.slice(0, 8)}...${address.slice(-8)}`
+const setCharAt = (str, index, chr) => {
+  if (index > str.length - 1) return str
+  return str.substr(0, index) + chr + str.substr(index + 1)
+}
+
+// helper routine to use complicated regex to
+// inject commas into a number at the right places.
+const commafy = (commas, n) => {
+  let ns = n.toString()
+  if (commas) {
+    ns = ns.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   }
-  return address
+  return ns
 }
 
 // This function does a purely text-based rounding algorithm on things that look
@@ -46,112 +45,120 @@ const roundUp = (n, carry) => {
   return roundUp(n.slice(0, n.length - 1), last === '9') + newlast
 }
 
-/**
- * This takes a number of napu, which should be an integer, plus a
- * precision (number of decimal digits) and returns a string
- * which is the human-readable value in ndau, rounded to that
- * number of decimal digits. No floating point math is used.
- */
-module.exports.formatNapuForDisplay = (napu, digits, commas) => {
-  // trap for the unwary:
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
-  // This function is being used here to say "is digits usable as a numeric value?"
-  // the default value is 3 digits.
-  if (isNaN(digits)) {
-    digits = 3
-  }
-  // fix digits if it's out of range (only 0-8 are permitted)
-  if (digits < 0) {
-    digits = 0
-  }
-  if (digits > 8) {
-    digits = 8
-  }
+module.exports = {
+  /**
+   * This function will take a string passed in and truncate
+   * it down to 19 characters if it is more than that. This
+   * was created for ndau addresses as they are 48 characters
+   * and tend to cause issues when displaying them in the UI.
+   *
+   * @param {string} address ndau address to be truncated
+   * @returns {string} truncated string
+   */
+  truncateAddress: address => {
+    if (address && address.length > 19) {
+      return `${address.slice(0, 8)}...${address.slice(-8)}`
+    }
+    return address
+  },
 
-  // if the number is negative, we'll need a sign, so get that
-  // out of the way first and then work in positive numbers
-  let sign = ''
-  if (napu < 0) {
-    napu = -napu
-    sign = '-'
-  }
+  /**
+   * This takes a number of napu, which should be an integer, plus a
+   * precision (number of decimal digits) and returns a string
+   * which is the human-readable value in ndau, rounded to that
+   * number of decimal digits. No floating point math is used.
+   */
+  formatNapuForDisplay: (napu, digits, commas) => {
+    // trap for the unwary:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
+    // This function is being used here to say "is digits usable as a numeric value?"
+    // the default value is 3 digits.
+    if (isNaN(digits)) {
+      digits = 3
+    }
+    // fix digits if it's out of range (only 0-8 are permitted)
+    if (digits < 0) {
+      digits = 0
+    }
+    if (digits > 8) {
+      digits = 8
+    }
 
-  let ndau = Math.floor(napu / constants.QUANTA_PER_UNIT)
-  let frac = napu % constants.QUANTA_PER_UNIT
+    // if the number is negative, we'll need a sign, so get that
+    // out of the way first and then work in positive numbers
+    let sign = ''
+    if (napu < 0) {
+      napu = -napu
+      sign = '-'
+    }
 
-  if (digits === 0 && frac >= constants.QUANTA_PER_UNIT / 2) {
-    ndau = ndau + 1
-  }
+    let ndau = Math.floor(napu / constants.QUANTA_PER_UNIT)
+    let frac = napu % constants.QUANTA_PER_UNIT
 
-  // if digits === 0 then we want a rounded number of ndau,
-  // so let's do that before messing with frac
-  if (digits === 0) {
-    return sign + commafy(commas, ndau)
-  }
+    if (digits === 0 && frac >= constants.QUANTA_PER_UNIT / 2) {
+      ndau = ndau + 1
+    }
 
-  // now we have sign and ndau and decimal point
-  // now format the fractional value
-  // turn it into an 8-digit string with leading zeros if needed.
-  let fs = ('00000000' + frac.toString()).slice(-8)
-  // trim it to the right length
-  let fracs = fs.slice(0, digits)
-  let result = sign + commafy(commas, ndau) + '.' + fracs.toString()
-  // if we need all 8 digits we're done, just return it
-  if (digits === 8) {
-    return result
-  }
+    // if digits === 0 then we want a rounded number of ndau,
+    // so let's do that before messing with frac
+    if (digits === 0) {
+      return sign + commafy(commas, ndau)
+    }
 
-  // otherwise, we need to try rounding it
-  let nextdigit = fs[digits]
-  return roundUp(result, nextdigit >= '5')
-}
+    // now we have sign and ndau and decimal point
+    // now format the fractional value
+    // turn it into an 8-digit string with leading zeros if needed.
+    let fs = ('00000000' + frac.toString()).slice(-8)
+    // trim it to the right length
+    let fracs = fs.slice(0, digits)
+    let result = sign + commafy(commas, ndau) + '.' + fracs.toString()
+    // if we need all 8 digits we're done, just return it
+    if (digits === 8) {
+      return result
+    }
 
-// helper routine to use complicated regex to
-// inject commas into a number at the right places.
-const commafy = (commas, n) => {
-  let ns = n.toString()
-  if (commas) {
-    ns = ns.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-  }
-  return ns
-}
+    // otherwise, we need to try rounding it
+    let nextdigit = fs[digits]
+    return roundUp(result, nextdigit >= '5')
+  },
 
-// This parses a string intended to be a number of ndau and converts it
-// safely to napu without using floating point. It returns the value of
-// the input string in napu. It will throw a string explaining the problem
-// if the value is unparseable.
-module.exports.parseNdau = s => {
-  // in order to have repeatable groupings, it's clearest to use multiple
-  // regexps to support both '1.' and '.1' as valid numbers.
-  const numpat1 = /^([-+]?)([0-9]+)(\.([0-9]*))?$/
-  const numpat2 = /^([-+]?)([0-9]*)?(\.([0-9]+))$/
-  // coerce it to a string and strip spaces and commas
-  s = String(s)
-    .trim()
-    .replace(/,/g, '')
-  let parts = numpat1.exec(s)
-  if (!parts) {
-    parts = numpat2.exec(s)
-  }
-  if (!parts) {
-    // neither pattern matched
-    throw Error(s + ' is not a number')
-  }
-  // ok, we know all the parts are valid, put it together
-  let sign = parts[1]
-  let whole = parts[2]
-  let frac = parts[4]
+  // This parses a string intended to be a number of ndau and converts it
+  // safely to napu without using floating point. It returns the value of
+  // the input string in napu. It will throw a string explaining the problem
+  // if the value is unparseable.
+  parseNdau: s => {
+    // in order to have repeatable groupings, it's clearest to use multiple
+    // regexps to support both '1.' and '.1' as valid numbers.
+    const numpat1 = /^([-+]?)([0-9]+)(\.([0-9]*))?$/
+    const numpat2 = /^([-+]?)([0-9]*)?(\.([0-9]+))$/
+    // coerce it to a string and strip spaces and commas
+    s = String(s)
+      .trim()
+      .replace(/,/g, '')
+    let parts = numpat1.exec(s)
+    if (!parts) {
+      parts = numpat2.exec(s)
+    }
+    if (!parts) {
+      // neither pattern matched
+      throw Error(s + ' is not a number')
+    }
+    // ok, we know all the parts are valid, put it together
+    let sign = parts[1]
+    let whole = parts[2]
+    let frac = parts[4]
 
-  let napu = 0
-  if (whole) {
-    napu += parseInt(whole) * constants.QUANTA_PER_UNIT
+    let napu = 0
+    if (whole) {
+      napu += parseInt(whole) * constants.QUANTA_PER_UNIT
+    }
+    if (frac) {
+      frac = (frac + '00000000').slice(0, 8)
+      napu += parseInt(frac)
+    }
+    if (sign == '-') {
+      napu = -napu
+    }
+    return napu
   }
-  if (frac) {
-    frac = (frac + '00000000').slice(0, 8)
-    napu += parseInt(frac)
-  }
-  if (sign == '-') {
-    napu = -napu
-  }
-  return napu
 }
