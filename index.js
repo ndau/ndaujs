e099740002a9a3567adfa54f956c719087a90430
fet <fetmar@gmail.com>
2019-11-20 11:14:08 -0500

    change `module.exports` to `export default` and remove old exports

--- index.js
+++ index.js
@@ -22,24 +22,7 @@ i18next.init({
   }
 })
 
-module.exports = {
-  // These functions existed in the first release of ndaujs.
-  // These methods are deprecated as of version 1.3.0 in place
-  // of the newer format described below
-
-  /**
-   * @deprecated as of version 1.3.0
-   */
-  truncateAddress: address.truncateAddress,
-  /**
-   * @deprecated as of version 1.3.0
-   */
-  formatNapuForDisplay: address.formatNapuForDisplay,
-  /**
-   * @deprecated as of version 1.3.0
-   */
-  parseNdau: address.parseNdau,
-
+export default {
   // In versions >1.3.0 here is where you will be exposing your
   // classes/helpers that are within the src folder. At present
   // we only support ES6 consumers as the code pooped down is not
